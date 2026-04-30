071fc911a22ed9f9f78cac0b2095a6a58fe1d9a7
Rae <aldrichpadilla404@gmail.com>
2026-04-30 15:54:18 +0800

    main file done

--- P2/main.py
+++ P2/main.py
@@ -1,2 +1,7 @@
 from students_grade_tracker import GradeTracker
 
+tracker = GradeTracker()
+top = tracker.search_top_student('students.txt')
+
+if top:
+    print(f"The Top student is {top.name} with a GWA of {top.gwa}")
