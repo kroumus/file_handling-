26c8adaf30cebb722fd9e232f72f01af32fe14da
Rae <aldrichpadilla404@gmail.com>
2026-04-30 22:38:11 +0800

    integer_processor module done

--- P4/integer_processor.py
+++ P4/integer_processor.py
@@ -1,5 +1,22 @@
 class IntegerProcessor:
-    def __init__(self, input_file):
-        self.input_file = input_file
+    def __init__(self, file_name):
+        self.file_name = file_name
     
-    def 
+    def process_numbers(self):
+        try:
+            with open(self.input_file, 'r') as file:
+                data = file.readlines()
+            
+            with open('double.txt', 'w') as double, open ('triple.txt', 'w') as triple:
+                for line in data:
+                    try:
+                        value = int(line.strip())
+                        if value % 2 == 0:
+                            double.write(f"{value**2}\n")
+                        else:
+                            triple.write(f"{value**3}\n")
+                    except ValueError:
+                        print(f"Skipping invalid data: {line.strip()}")
+        
+        except Exception as error:
+            print(f"An unexpected error occurred: {error}")
