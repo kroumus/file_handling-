e18101ddcc21590e0479d44185db4d287964e5ea
Rae <aldrichpadilla404@gmail.com>
2026-04-30 15:40:44 +0800

    student_score module done

--- P2/students_score.py
+++ P2/students_score.py
@@ -3,4 +3,14 @@ class Student:
         self.name = name
         self.gwa = float(gwa)
 class GradeTracker:
-    def 
+    def search_top_student(self, file_name):
+        top_student = None
+
+        with open(file_name, 'r') as file:
+            for line in file:
+                name, gwa = line.strip().split(',')
+                current_student = Student(name, gwa)
+        
+                if top_student is None or current_student.gwa < top_student.gwa:
+                    top_student = current_student
+        return top_student
