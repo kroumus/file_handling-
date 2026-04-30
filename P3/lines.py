d6a3a8f9f10b501f07057bf74057efba6012ddc7
Rae <aldrichpadilla404@gmail.com>
2026-04-30 16:34:13 +0800

    defined write_multiple_lines

--- P3/lines.py
+++ P3/lines.py
@@ -2,4 +2,13 @@ class MyLife:
     def __init__(self, file_name="MyLife.txt"):
         self.file_name = file_name
 
-    
+    def write_multiple_lines(self):
+        with open(self.file_name, 'w') as file:
+            while True:
+                line = input("Enter Line: ")
+                file.write(line + "\n")
+
+                choice = input("Are there more lines? y/n").lower()
+                if choice != 'y':
+                    break
+        print(f"Lines saved to {self.file_name}")
