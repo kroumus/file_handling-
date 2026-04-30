706cd7b4aa9421ea5c785a4e4e25fba88a871486
Rae <aldrichpadilla404@gmail.com>
2026-04-30 14:46:38 +0800

    defined main

--- P1/main.py
+++ P1/main.py
@@ -1,2 +1,9 @@
 from number_processor import NumberProcessor
 
+def main():
+    processor = NumberProcessor('numbers.txt')
+    if processor.separate_numbers():
+        print("Verified, Files 'even.txt' and 'odd.txt' have been created.")
+    else:
+        print("Error: numbers.txt' was not found.")
+
