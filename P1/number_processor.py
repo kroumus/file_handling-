47bf712de219c67e2074b9f5f14ea7741d79ee23
Rae <aldrichpadilla404@gmail.com>
2026-04-30 14:38:46 +0800

    even.txt and odd.txt

--- P1/number_processor.py
+++ P1/number_processor.py
@@ -6,3 +6,11 @@ class NumberProcessor:
         try:
             with open(self.input_file, 'r') as file:
                 nums =[int(n) for n in file.read().split()]
+        
+        even_nums = [str(n) for n in nums if n % 2 == 0]
+        odd_nums= [str(n) for n in nums if n % 2 != 0]
+
+        self.write_to_file('even.txt', even_nums)
+        self.write_to_file('odd.txt', odd_nums)
+
+        
